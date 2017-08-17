0a973cd8d2bd5a51c5e533ff716029e6c0f0ba00
Shane Fontaine <shane6fontaine@gmail.com>
2017-08-17 16:09:15 -0700

    Updatd ERC20 standard

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,29 +2,29 @@ pragma solidity ^0.4.11;
 
 import './SafeMath.sol';
 
-///**
-// * @title ERC20Basic
-// * @dev Simpler version of ERC20 interface
-// * @dev see https://github.com/ethereum/EIPs/issues/179
-// */
-//contract ERC20Basic {
-//    uint256 public totalSupply;
-//    function balanceOf(address who) constant returns (uint256);
-//    function transfer(address to, uint256 value) returns (bool);
-//    event Transfer(address indexed from, address indexed to, uint256 value);
-//}
-//
-///**
-// * @title ERC20 interface
-// * @dev see https://github.com/ethereum/EIPs/issues/20
-// */
-//contract ERC20 is ERC20Basic {
-//    function allowance(address owner, address spender) constant returns (uint256);
-//    function transferFrom(address from, address to, uint256 value) returns (bool);
-//    function approve(address spender, uint256 value) returns (bool);
-//    event Approval(address indexed owner, address indexed spender, uint256 value);
-//}
-//
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+    uint256 public totalSupply;
+    function balanceOf(address who) constant returns (uint256);
+    function transfer(address to, uint256 value) returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) returns (bool);
+    function approve(address spender, uint256 value) returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 /**
  * @title Basic token
  * @dev Basic version of StandardToken, with no allowances.
@@ -33,16 +33,6 @@ contract BasicToken {
     using SafeMath for uint256;
 
     mapping(address => uint256) balances;
-
-//    uint256 public totalSupply;
-    event Transfer(address indexed from, address indexed to, uint256 value);
-//
-//    function allowance(address owner, address spender) constant returns (uint256);
-//    function transferFrom(address from, address to, uint256 value) returns (bool);
-//    function approve(address spender, uint256 value) returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-
 
     /**
     * @dev transfer token for a specified address
