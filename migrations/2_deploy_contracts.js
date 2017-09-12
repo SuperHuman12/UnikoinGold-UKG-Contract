ffcabff2e57a1fe492512bae347ee1fb9d43b816
Shane Fontaine <shane6fontaine@gmail.com>
2017-09-12 14:34:34 -0700

    updated migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,25 +1,18 @@
 var SafeMath = artifacts.require("./SafeMath.sol");
 var Ownable = artifacts.require("./Ownable.sol");
-
 var BasicToken = artifacts.require("./BasicToken.sol");
-
 var StandardToken = artifacts.require("./StandardToken.sol");
 var TokenDistribution = artifacts.require("./TokenDistribution.sol");
 
 var BigNumber = require("bignumber.js");
+var curr_time = Math.floor(new Date().getTime() / 1000);
 
 module.exports = function(deployer, network, accounts) {
     deployer.deploy(SafeMath);
     deployer.deploy(Ownable);
-
     deployer.deploy(BasicToken);
     deployer.deploy(StandardToken);
-    var params;
-    if (network == "live") {
-        // TODO: Add params for live script
-    } else {
-        // TODO: Add params for test script
-    }
+
     deployer.link(SafeMath, TokenDistribution);
-    deployer.deploy(TokenDistribution, accounts[0], new BigNumber(1), new BigNumber(100000));
+    deployer.deploy(TokenDistribution, accounts[0], accounts[1], curr_time  + 1*60, curr_time +  + 2*60);
 };
