59a968269b3356fe0785a69c2ba037512b8ea91c
Shane Fontaine <shane6fontaine@gmail.com>
2017-10-09 10:20:56 -0700

    Updated deployment and migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,20 @@ var TokenDistribution = artifacts.require("./TokenDistribution.sol");
 
 now = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
 
-const OWNER = '0x00d6B2cC8B0963AF7eDA3815b4E798bC1B69B527';
-const PROXY_ADDRESS = '0x57dd47E82548E535A60eb14EfC6611f64A80b899';
+const MINUTE = 60;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+const YEAR = 365 * DAY;
+
+const OWNER = web3.eth.coinbase;
+const PROXY_ADDRESS = '0x988eE09bdd9b110C08103159610ef1f01319eAFF';
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(ParticipantAdditionProxy);
-    deployer.deploy(TokenDistribution, OWNER, PROXY_ADDRESS, now, now + 300, now + 500);
+    if (network == 'kovanproxy') {
+        deployer.deploy(ParticipantAdditionProxy);
+    }
+
+    if (network == 'kovandistribution') {
+        deployer.deploy(TokenDistribution, OWNER, PROXY_ADDRESS, now + (2 * DAY), now + (5 * DAY), now + (3 * YEAR));
+    }
 };
--- truffle.js
+++ truffle.js
@@ -3,12 +3,19 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*", // Match any network id
+      network_id: "*" // Match any network id
     },
-    parity: {
+    kovanproxy: {
       host: "localhost",
       port: 8545,
-      network_id: "*", // Match any network id
+      network_id: "42", // Kovan network_id
+      gas: 6993150
+    },
+    kovandistribution: {
+      host: "localhost",
+      port: 8545,
+      network_id: "42", // Kovan network_id
+      gas: 6900000
     }
   }
 };
