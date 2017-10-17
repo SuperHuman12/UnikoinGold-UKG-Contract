2c08c581b7e1efaa6bfdbd4790bff39b9a56f496
Shane Fontaine <shane6fontaine@gmail.com>
2017-10-17 00:24:55 -0700

    Updated migrations to deploy at separate times

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,27 +1,25 @@
 var ParticipantAdditionProxy = artifacts.require("./ParticipantAdditionProxy.sol");
 var TokenDistribution = artifacts.require("./TokenDistribution.sol");
+var UnikoinGold = artifacts.require("./UnikoinGold.sol");
 
 now = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
 
 const MINUTE = 60;
 const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
-const YEAR = 365 * DAY;
 
-const OWNER = web3.eth.coinbase;
-const PROXY_ADDRESS = '0x988eE09bdd9b110C08103159610ef1f01319eAFF';
+const PROXY_ADDRESS = web3.eth.coinbase;
+const UKG_FUND =  web3.eth.coinbase;
 
 module.exports = function(deployer, network, accounts) {
-    if (network == 'kovanproxy') {
+
+    if (network == 'mainproxy') {
         deployer.deploy(ParticipantAdditionProxy);
     }
 
-    if (network == 'kovandistribution') {
-        deployer.deploy(TokenDistribution, OWNER, PROXY_ADDRESS, now + (2 * DAY), now + (5 * DAY));
+    if (network == 'maindistribution') {
+        deployer.deploy(TokenDistribution, PROXY_ADDRESS, now + (2 * DAY), now + (5 * DAY)).then(function() {
+            return deployer.deploy(UnikoinGold, TokenDistribution.address, UKG_FUND);
+        });
     }
-
-
-    deployer.deploy(ParticipantAdditionProxy);
-    deployer.deploy(TokenDistribution, OWNER, PROXY_ADDRESS, now + (2 * DAY), now + (5 * DAY));
-
 };
